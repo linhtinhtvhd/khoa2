19c63874fd6263a0c6281bc7138fde1543a0ee6f
linhtinhthhd <linhtinhthhd@gmail.com>
2022-06-23 21:51:00 +0700

    03

--- 03/src/component/ToDoList.jsx
+++ 03/src/component/ToDoList.jsx
@@ -5,37 +5,22 @@ export const ToDoList = () => {
   return (
     <div className='trafficsigns'>
         <button className='btn' onClick={function(e){
-              if (count >3){
-                count = 1
-              }
-              setCount(count+1)
-           let m = e.currentTarget.parentNode.querySelector('.input-radio')
-              if(count!==1){
-                m.querySelector('.red').classList.add('back-none')
-              }
-              else{
-                m.querySelector('.red').classList.remove('back-none')
-              }
-              if(count!==2){
-                m.querySelector('.yellow').classList.add('back-none')
-              }
-              else{
-                m.querySelector('.yellow').classList.remove('back-none')
-              }
-              if(count!==3){
-                m.querySelector('.green').classList.add('back-none')
-              }
-              else{
-                m.querySelector('.green').classList.remove('back-none')
-              }
+            
+              setCount((prev)=>{
+                if(prev===3){
+                  prev = 0
+                }
+                return ++prev
+              })
+          
            console.log(count)
         }}>Next</button>
       
         <div className='input-radio'>
         
-            <div className={`color red `}></div>
-            <div className={`color yellow back-none`}></div>
-            <div className='color green back-none'></div>
+            <div className={`color white ${count ===1 && 'red' }`}></div>
+            <div className={`color white ${count ===2 && 'yellow' }`}></div>
+            <div className={`color white ${count ===3 && 'green' }`}></div>
           
         </div>
     </div>
