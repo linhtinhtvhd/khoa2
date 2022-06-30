8154ecff879251bbcb91d48665641d6896467fa9
linhtinhthhd <linhtinhthhd@gmail.com>
2022-06-30 13:50:58 +0700

    [Linh] bt buoi 4

--- src/component/ToDoList.jsx
+++ src/component/ToDoList.jsx
@@ -1,29 +1,14 @@
-import React, { useState } from 'react'
+import React from 'react'
+import {Header} from './Header'
+import {Footer} from './Footer'
+import {Form} from './Form'
 
 export const ToDoList = () => {
-    let [count,setCount] = useState(2)
   return (
-    <div className='trafficsigns'>
-        <button className='btn' onClick={function(e){
-            
-              setCount((prev)=>{
-                if(prev===3){
-                  prev = 0
-                }
-                return ++prev
-              })
-          
-           console.log(count)
-        }}>Next</button>
-      
-        <div className='input-radio'>
-        
-            <div className={`color white ${count ===1 && 'red' }`}></div>
-            <div className={`color white ${count ===2 && 'yellow' }`}></div>
-            <div className={`color white ${count ===3 && 'green' }`}></div>
-          
-        </div>
-    </div>
-    
+   <div className='body-netflix'>
+            <Header/>
+            <Form/>
+            <Footer/>
+   </div>
   )
 }
